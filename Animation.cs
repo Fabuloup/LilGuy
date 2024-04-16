3a3ded870ab435270d6024282214cde4cb1840a8
frichard0 <frichard0@solutec.fr>
2024-04-16 17:48:36 +0200

    Added first version of LilGuy

---
+++ Animation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lilguy
+{
+    public class Animation
+    {
+        private Stopwatch _stopwatch = new Stopwatch();
+        private List<int> _keyframes = new List<int>();
+
+        public Animation(List<int> keyframes)
+        {
+            keyframes.Sort();
+            _keyframes = keyframes;
+        }
+
+        public void Start()
+        {
+            _stopwatch.Start();
+        }
+
+        public void Stop()
+        {
+            _stopwatch.Stop();
+        }
+
+        public void Reset()
+        {
+            _stopwatch.Reset();
+        }
+
+        public void Restart()
+        {
+            _stopwatch.Restart();
+        }
+
+        public int GetKeyframe()
+        {
+            int keyframe = _keyframes.Where(k => k < _stopwatch.ElapsedMilliseconds).Count();
+            if(keyframe == _keyframes.Count)
+            {
+                _stopwatch.Restart();
+            }
+            return keyframe;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using Brushes = System.Windows.Media.Brushes;
+using Color = System.Drawing.Color;
+using Point = System.Windows.Point;
+
+namespace lilguy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<string> heads = new List<string>
+        {
+            "^_^",
+            "^°^",
+            "^o^",
+            "°-°",
+            "+_+",
+            "O.O",
+            "◉‿◉",
+            "◉‿◉",
+            "◕‿‿◕",
+            "◕ヮ◕",
+            "≧︿≦"
+        };
+        private Thread updateThread;
+        private bool running = true;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Topmost = true;
+
+            this.MouseDown += MouseDownHandler;
+
+            updateThread = new Thread(new ThreadStart(UpdateLilGuy));
+            updateThread.Start();
+        }
+
+        private void MouseDownHandler(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void UpdateLilGuy()
+        {
+            Random rand = new Random();
+            Animation lilguySnoresAnimation = new Animation(new List<int> {
+                0,      // Closed mouth
+                1400,    // Transition mouth
+                1500,    // Open mouth
+                2800,    // Transition mouth
+                3000    // Last Keyframe
+            });
+            lilguySnoresAnimation.Start();
+
+            while (running)
+            {
+                // Check if the window is on top
+                if (!this.IsActive)
+                {
+                    this.Activate(); // Bring the window to the foreground
+                }
+
+                // Check if it's time to display Pause message
+                DateTime now = DateTime.Now;
+                if ((now.Hour == 10 && now.Minute == 30) || (now.Hour == 16 && now.Minute == 0))
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        lilguyTextBox.Text = heads[rand.Next(heads.Count)] + $"  -- {((rand.Next(0,2) == 0) ? "Pause" : "c[_]")} ?";
+                    });
+
+                    // Pause for 15 minutes
+                    Thread.Sleep(15 * 60 * 1000);
+                }
+                else
+                {
+                    // Animate the lilguy snoring
+                    Dispatcher.Invoke(() =>
+                    {
+                        switch (lilguySnoresAnimation.GetKeyframe())
+                        {
+                            case 1:
+                                lilguyTextBox.Text = "U.U";
+                                break;
+                            case 2:
+                                lilguyTextBox.Text = "UoU";
+                                break;
+                            case 3:
+                                lilguyTextBox.Text = "UOU";
+                                break;
+                            case 4:
+                                lilguyTextBox.Text = "UoU";
+                                break;
+                            default:
+                                lilguyTextBox.Text = "U.U";
+                                break;
+                        }
+                    });
+
+                    // Wait for 50ms
+                    Thread.Sleep(50);
+                }
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            running = false;
+            updateThread.Join();
+        }
+    }
+}
